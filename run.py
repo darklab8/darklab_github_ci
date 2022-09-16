d58895d42ef3559cd541284268c1e038ea457650
dd84ai <dd84ai@gmail.com>
2022-09-16 18:15:32 +0200

    fix: adjust running to account already being configured

--- run.py
+++ run.py
@@ -26,19 +26,28 @@ parser.add_argument(
 args = parser.parse_args()
 print("starting to parsed args")
 
+print("trying to configure")
 child = pexpect.spawn(
     f'./config.sh --url {args.url} --token {args.token}',
     encoding='utf-8',
     logfile=sys.stdout,
 )
-print("lanched process")
-child.expect('the name of the runner group') # Default
-child.sendline("\n")
-child.expect('name of runner') # random ID
-child.sendline("\n")
-child.expect('Enter any additional labels')
-child.sendline("\n")
-child.expect('name of work folder') # _work
-child.sendline("\n")
+print("expecting to make a choice")
+result = child.expect(['the name of the runner group', 'already configured']) # Default
 
+print("making a choice")
+if result == 0:
+    print("python3: configuring runner")
+    child.sendline("\n")
+    child.expect('name of runner') # random ID
+    child.sendline("\n")
+    child.expect('Enter any additional labels')
+    child.sendline("\n")
+    child.expect('name of work folder') # _work
+    child.sendline("\n")
+else:
+    print("python3: runner is already configured")
+
+print("python3: running listener")
 subprocess.run("./run.sh", shell=True, check=True)
+
